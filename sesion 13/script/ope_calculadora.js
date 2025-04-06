aec512d5d887ee434fcce573b812c4f12e0352e8
dayssonR <dayjuegos13@gmail.com>
2025-04-06 10:31:27 -0500

    Add files via upload

---
+++ sesion 13/script/ope_calculadora.js
@@ -0,0 +1,7 @@
+//optener la referencia de la pantalla
+let pantalla = document.getElementById('pantalla');
+
+//agregamos los valores
+function agregar(valor){
+    pantalla.value = pantalla + valor;
+}
